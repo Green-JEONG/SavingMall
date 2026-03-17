07536a428d863022f0117819763bfeddf29924a2
Horok <th2gr22n@gmail.com>
2026-03-17 13:25:32 +0900

    feat: @Slf4j 추가, 시작 후 debug 로그 출력

--- hanaro/src/main/java/com/hana8/hanaro/HanaroApplication.java
+++ hanaro/src/main/java/com/hana8/hanaro/HanaroApplication.java
@@ -1,15 +1,19 @@
 package com.hana8.hanaro;
 
 import com.hana8.hanaro.config.AppProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties({AppProperties.class})
 public class HanaroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HanaroApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(HanaroApplication.class, args);
+        log.debug("application started, beanDefinitionCount={}", ctx.getBeanDefinitionCount());
     }
 }
