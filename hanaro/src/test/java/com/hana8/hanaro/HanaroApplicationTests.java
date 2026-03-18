bccfe38c31601ccae371b8575f7ed0b5364180fc
Horok <th2gr22n@gmail.com>
2026-03-18 17:34:16 +0900

    fix: coverage 리포트 성능 개선 - QueryDSL 생성 클래스 jacoco 대상에서 제외, main() 호출 테스트 추가로 루트 패키지 직접 커버

--- hanaro/src/test/java/com/hana8/hanaro/HanaroApplicationTests.java
+++ hanaro/src/test/java/com/hana8/hanaro/HanaroApplicationTests.java
@@ -1,13 +1,35 @@
 package com.hana8.hanaro;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.mockito.MockedStatic;
 
 @SpringBootTest
 class HanaroApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainRunsSpringApplication() {
+        try (MockedStatic<SpringApplication> springApplication = mockStatic(SpringApplication.class)) {
+            ConfigurableApplicationContext context = mock(ConfigurableApplicationContext.class);
+            when(context.getBeanDefinitionCount()).thenReturn(1);
+
+            springApplication.when(() -> SpringApplication.run(HanaroApplication.class, new String[]{}))
+                    .thenReturn(context);
+
+            HanaroApplication.main(new String[]{});
+
+            springApplication.verify(() -> SpringApplication.run(HanaroApplication.class, new String[]{}));
+        }
     }
 
 }
